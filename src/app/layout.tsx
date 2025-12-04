e59b133733a4e963b5e56d7fef44465391f45482
Alan Liu <liualan998@gmail.com>
2025-12-04 10:56:52 -0500

    [fix] image wrangling

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,22 @@
+// builtin
+
+// external
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Caveat, Nothing_You_Could_Do } from "next/font/google";
+
+// internal
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+const caveat = Caveat({
+  variable: "--font-caveat",
+  subsets: ["latin"]
+})
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const nothingYouCouldDo = Nothing_You_Could_Do({
+  variable: "--font-nothing-you-could-do",
+  weight: "400",
+  subsets: ["latin"]
+})
 
 export const metadata: Metadata = {
   title: "Alan Liu",
@@ -25,7 +31,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${caveat.variable} ${nothingYouCouldDo.variable} antialiased`}
       >
         {children}
       </body>
