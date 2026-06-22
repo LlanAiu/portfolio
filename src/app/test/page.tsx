db33534f6cf1aca01cb5d20f567a1fb69a967886
Alan Liu <liualan998@gmail.com>
2026-06-21 22:22:09 -0400

    [feature] diff fade + rewrite

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,36 +1,79 @@
 'use client';
-import ParallelAnimation from "@/components/animations/parallel-animation";
+
 // builtin
 
 // external
+import { useRef, useState } from 'react';
+import { TegakiRenderer, type TegakiRendererHandle } from 'tegaki';
+import bundle from 'tegaki/fonts/caveat';
 
 // internal
-import SequentialAnimation from "@/components/animations/sequential-animation";
-import TegakiList from "@/components/animations/tegaki/tegaki-list";
-import TegakiText from "@/components/animations/tegaki/tegaki-text";
-
-
-const listItems = [
-    "First animated point",
-    "Second animated point",
-];
-
-
-const listItems2 = [
-    "1st animated point",
-];
 
 export default function TestPage() {
+    const ref = useRef<TegakiRendererHandle>(null);
+    const ref2 = useRef<TegakiRendererHandle>(null);
 
+    const [count, setCount] = useState(0);
+    const [playFirst, setPlayFirst] = useState(true);
+
+    async function computeNewTimeline() {
+        if (!ref.current?.engine || !ref2.current?.engine) {
+            return;
+        }
+        ref.current.engine.pause();
+        ref2.current.engine.pause();
+        const engine = (playFirst) ? ref2.current.engine : ref.current.engine;
+
+        const newDisplay = `Count: ${count + 1}`;
+        const currentTimeline = engine.timeline.entries;
+        const newTimeline = engine.computeTimeline(newDisplay).entries;
+
+        let index = 0;
+        while (index < currentTimeline.length && index < newTimeline.length) {
+            if (currentTimeline[index].char !== newTimeline[index].char) {
+                break;
+            } else {
+                index += 1;
+            }
+        }
+
+        engine.seek(newTimeline[index].offset);
+        setPlayFirst(prev => !prev);
+        await new Promise(resolve => setTimeout(resolve, 300));
+        setCount(s => s + 1);
+        engine.play();
+    }
+
+    function debugTimeline() {
+        console.log(ref.current?.engine?.timeline.entries)
+    }
+
+    const displayString = `Count: ${count}`;
     return (
-        <SequentialAnimation id="sequence1">
-            <ParallelAnimation id="parallel1">
-                <TegakiList id="list1" items={listItems} />
-                <TegakiList id="list2" items={listItems2} />
-            </ParallelAnimation>
-            <TegakiText id="text">
-                Yay all done!
-            </TegakiText>
-        </SequentialAnimation>
-    )
+        <>
+            <div>
+                <TegakiRenderer
+                    ref={ref}
+                    font={bundle}
+                    time={{ mode: 'uncontrolled', speed: 1 }}
+                    style={{ fontSize: 48, transition: 'opacity 0.3s ease-in-out', opacity: (playFirst) ? 1 : 0 }}
+                >
+                    {displayString}
+                </TegakiRenderer>
+
+                <TegakiRenderer
+                    ref={ref2}
+                    font={bundle}
+                    time={{ mode: 'uncontrolled', speed: 1 }}
+                    style={{ position: "absolute", top: 0, fontSize: 48, transition: 'opacity 0.3s ease-in-out', opacity: (playFirst) ? 0 : 1 }}
+                >
+                    {displayString}
+                </TegakiRenderer>
+
+            </div>
+
+            <button type="button" onClick={computeNewTimeline}>+1</button>
+            <button type="button" onClick={debugTimeline}>Debug</button>
+        </>
+    );
 }
