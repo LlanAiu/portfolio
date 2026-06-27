4d18154583b9f6593c82540b6bd8c06d7a2d2bf0
Alan Liu <liualan998@gmail.com>
2026-06-27 15:26:34 -0400

    [feature] tegaki wrapper component

--- src/components/animations/tegaki/tegaki-wrapper.tsx
+++ src/components/animations/tegaki/tegaki-wrapper.tsx
@@ -1,5 +1,26 @@
 // builtin
 
 // external
+import { cloneElement, type ReactElement } from "react";
 
 // internal
+import { TegakiContext, type TegakiContextProvider } from "@/lib/animation/tegaki-context";
+import type { TimedAnimation } from "@/lib/animation/timed-animation";
+
+
+interface TegakiWrapperProps extends TimedAnimation, TegakiContextProvider {
+    children: ReactElement<TimedAnimation>;
+}
+
+export default function TegakiWrapper({ children, onComplete, context }: TegakiWrapperProps) {
+
+    return (
+        <TegakiContext value={context ?? {}}>
+            {
+                cloneElement(children, {
+                    onComplete
+                })
+            }
+        </TegakiContext>
+    );
+}
