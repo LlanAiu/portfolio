6c7d57164ef6d5babbb92fc10be937aa2388e9d3
Alan Liu <liualan998@gmail.com>
2026-06-28 16:26:43 -0400

    [feature] redid about but not styled

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -16,6 +16,9 @@ import roboticsImage from '../../../public/Robotics.jpg';
 import cardsImage from '../../../public/Cards.jpg';
 import Navigation from '@/components/layout/nav-bar';
 import { TextChangeDelay, TextInitialX, TextSwapFade } from '@/lib/animation/animation-utils';
+import SequentialAnimation from '@/components/animations/sequential-animation';
+import TegakiText from '@/components/animations/tegaki/tegaki-text';
+import RewritingTegakiText from '@/components/animations/tegaki/rewriting-tegaki-text';
 
 export default function AboutPage() {
     const [index, setIndex] = useState(0);
@@ -57,32 +60,32 @@ export default function AboutPage() {
 
     const descriptions = [
         {
-            text: 'am a stat and CS major',
+            text: 'I am a stat and CS major',
             image: defaultImage,
             dim: { width: 500, height: 600 }
         },
         {
-            text: 'am a student @ UNC-CH',
+            text: 'I am a student @ UNC-CH',
             image: bellTowerImage,
             dim: { width: 300, height: 500 }
         },
         {
-            text: 'love playing cards with friends and family',
+            text: 'I love playing cards with friends and family',
             image: cardsImage,
             dim: { width: 375, height: 350 }
         },
         {
-            text: 'am an FRC Alum',
+            text: 'I am an FRC Alum',
             image: FRCImage,
             dim: { width: 450, height: 300 }
         },
         {
-            text: 'am interested in machine learning and robotics',
+            text: 'I am interested in machine learning and robotics',
             image: roboticsImage,
             dim: { width: 400, height: 300 }
         },
         {
-            text: 'am a retired swimmer',
+            text: 'I am a retired swimmer',
             image: kouryImage,
             dim: { width: 275, height: 300 }
         }
@@ -92,74 +95,56 @@ export default function AboutPage() {
     const fade = TextSwapFade;
 
     return (
-        <div className='h-full w-full'>
-            <Navigation path='about' bg='#C4D7F2' text='#211C1F' bttn='#D1DBDA' hlght='#F0F7EE' />
+        <div
+            className='w-full h-full flex flex-col md:flex-row'
+        >
+            <div className='pt-8 px-4 md:inline-block flex-initial content-start space-y-8 md:space-y-24 text-center md:text-left'>
+                <SequentialAnimation id='about' context={{
+                    time: { mode: 'uncontrolled', delay: 0.4, speed: 2.5 }
+                }}>
+                    <TegakiText
+                        id='title'
+                        className='text-4xl md:text-6xl'
+                    >
+                        Mini Autobiography
+                    </TegakiText>
+                    <RewritingTegakiText
+                        id='fact'
+                        className='text-3xl md:text-5xl h-36'
+                    >
+                        {descriptions[index].text}
+                    </RewritingTegakiText>
+                </SequentialAnimation>
+            </div>
             <motion.div
-                className='w-full h-full'
-                animate={{ backgroundColor: '#AFDEDC', color: '#211C1F' }}
+                className='p-10 h-full w-full md:w-1/2 flex-auto content-start text-center md:text-left'
+                variants={text}
             >
                 <motion.div
-                    className='w-full h-full flex flex-col md:flex-row'
-                    animate='visible'
-                    initial='hidden'
-                    variants={text}
+                    animate={clsx({
+                        'out': swap,
+                        'in': !swap
+                    })}
+                    variants={fade}
                 >
-                    <motion.div className='pt-8 md:pt-28 px-4 md:pl-28 md:inline-block flex-initial content-start space-y-8 md:space-y-24 text-center md:text-left'>
-                        <motion.h1
-                            className='text-4xl md:text-6xl'
-                            variants={text}
-                        >
-                            <b>Mini Autobiography</b>
-                        </motion.h1>
-                        <motion.p
-                            className='text-4xl md:text-6xl max-w-full md:max-w-124'
-                            variants={text}
-                        >
-                            <b>I...</b>
-                            <span className='w-2 md:w-10'> </span>
-                            <motion.span
-                                className='text-3xl md:text-5xl leading-relaxed'
-                                animate={clsx({
-                                    'out': swap,
-                                    'in': !swap
-                                })}
-                                variants={fade}
-                            >
-                                {descriptions[index].text}
-                            </motion.span>
-                        </motion.p>
-                    </motion.div>
-                    <motion.div
-                        className='p-10 h-full w-full md:w-1/2 flex-auto content-start text-center md:text-left'
-                        variants={text}
-                    >
-                        <motion.div
-                            animate={clsx({
-                                'out': swap,
-                                'in': !swap
-                            })}
-                            variants={fade}
-                        >
-                            <div className='max-h-full h-full'>
-                                {!isMobile &&
-                                    <Image
-                                        src={descriptions[index].image}
-                                        alt='About me image'
-                                        loading='eager'
-                                        className='object-contain rounded-3xl'
-                                    />
-                                }
+                    <div className='max-h-full h-full'>
+                        {!isMobile &&
+                            <Image
+                                src={descriptions[index].image}
+                                alt='About me image'
+                                loading='eager'
+                                className='object-contain rounded-3xl'
+                            />
+                        }
 
-                                {isMobile &&
-                                    <Image
-                                        src={descriptions[index].image}
-                                        alt='About me image'
-                                        className='rounded-3xl'
-                                    />
-                                }
-                            </div>
-                        </motion.div>
-                    </motion.div>
+                        {isMobile &&
+                            <Image
+                                src={descriptions[index].image}
+                                alt='About me image'
+                                className='rounded-3xl'
+                            />
+                        }
+                    </div>
                 </motion.div>
             </motion.div>
         </div>
